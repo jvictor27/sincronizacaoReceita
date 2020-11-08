165c3be8de4e2005ff3a6990042a6aab1ce43de2
jvictor27 <jvictor451@gmail.com>
2020-11-08 15:05:01 -0300

    Ajuste na simulação de requisição

--- src/main/java/com/joaovictor/sicronizacaoreceita/services/impl/ReceitaServiceImpl.java
+++ src/main/java/com/joaovictor/sicronizacaoreceita/services/impl/ReceitaServiceImpl.java
@@ -1,14 +1,19 @@
 package com.joaovictor.sicronizacaoreceita.services.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.joaovictor.sicronizacaoreceita.domain.Conta;
+import com.joaovictor.sicronizacaoreceita.exceptions.ErroGenericoException;
+import com.joaovictor.sicronizacaoreceita.exceptions.ErroInternoException;
+import com.joaovictor.sicronizacaoreceita.exceptions.NaoAutenticadoException;
+import com.joaovictor.sicronizacaoreceita.exceptions.NaoAutorizadoException;
+import com.joaovictor.sicronizacaoreceita.exceptions.NaoEncontradoException;
+import com.joaovictor.sicronizacaoreceita.exceptions.RequisicaoInvalidaException;
+import com.joaovictor.sicronizacaoreceita.exceptions.ServicoIndisponivelException;
+import com.joaovictor.sicronizacaoreceita.exceptions.TempoExpiradoException;
 import com.joaovictor.sicronizacaoreceita.feing.utils.FeingErrorDecoderSimulado;
 import com.joaovictor.sicronizacaoreceita.services.ReceitaService;
 
@@ -20,7 +25,7 @@ public class ReceitaServiceImpl implements ReceitaService {
 		String resultado = null;
 		try {
 			// O bloco de código dentro do try simula uma chamda usando um feing client que se comunica com a API da Receita
-			int randomStatuCode;
+			int randomStatuCode = 404;
 	        // Simula tempo de resposta do serviço (entre 1 e 5 segundos)
 	        long wait = Math.round(Math.random() * 4000) + 1000;
 	        Thread.sleep(wait);
@@ -28,13 +33,14 @@ public class ReceitaServiceImpl implements ReceitaService {
 	        if (wait > 5000)
 	        	randomStatuCode = 503;
 	        // Simula cenário de erro no serviço (0,1% de erro)
-	        randomStatuCode = (int) Math.round(Math.random() * 1000);
-	        if (randomStatuCode < 200 && randomStatuCode > 299 && Objects.nonNull(HttpStatus.valueOf(randomStatuCode))) {
-	            new FeingErrorDecoderSimulado().decode(randomStatuCode);
+//	        randomStatuCode = (int) Math.round(Math.random() * 1000);
+	        if ((randomStatuCode < 200 || randomStatuCode > 299) && Objects.nonNull(HttpStatus.valueOf(randomStatuCode))) {
+	            throw new FeingErrorDecoderSimulado().decode(randomStatuCode);
 	        }
 	        
 	        resultado = "Sucesso";
-		} catch (RuntimeException e) {
+		} catch (RequisicaoInvalidaException | NaoAutorizadoException | NaoAutenticadoException | NaoEncontradoException 
+				| TempoExpiradoException | ErroInternoException | ServicoIndisponivelException | ErroGenericoException e) {
 			resultado = e.getMessage();
 		} finally {
 			conta.setResultado(resultado);
